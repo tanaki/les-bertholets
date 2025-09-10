3339ea94053fcd5a8ce030e73d7649acc2457167
Nicolas Pigelet <nipigelet@gmail.com>
2025-09-10 10:46:02 +0200

    wip responsive + labels

--- template-parts/case-studies/block-nav.php
+++ template-parts/case-studies/block-nav.php
@@ -30,8 +30,8 @@
                             <span class="thumbnail">
                                 <img src="<?php echo get_thumb('case_study', $prevId); ?>" alt="<?php echo $prevTitle; ?>" />
                             </span>
-                            <span>&Eacute;tude de Cas</span>
-                            <span>Précédente</span>
+                            <span><?php echo get_label('title_case_study'); ?></span>
+                            <span><?php echo get_label('btn_previous'); ?></span>
                             <img class="arrow" src="<?php echo get_template_directory_uri() . '/assets/img/arrow-left.svg'; ?>" alt="Etude de Cas - Précédente" />
                         </a>
                     </div>
@@ -40,8 +40,8 @@
                             <span class="thumbnail">
                                 <img src="<?php echo get_thumb('case_study', $nextId); ?>" alt="<?php echo $nextTitle; ?>" />
                             </span>
-                            <span>&Eacute;tude de Cas</span>
-                            <span>Suivante</span>
+                            <span><?php echo get_label('title_case_study'); ?></span>
+                            <span><?php echo get_label('btn_next'); ?></span>
                             <img class="arrow" src="<?php echo get_template_directory_uri() . '/assets/img/arrow-left.svg'; ?>" alt="Etude de Cas - Suivante" />
                         </a>
                     </div>
