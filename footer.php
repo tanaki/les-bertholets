6a526cb6a8765f21b6717f45f4b8d1c4b920911c
Nicolas Pigelet <nipigelet@gmail.com>
2025-12-12 19:09:46 +0100

    clean up api key

--- footer.php
+++ footer.php
@@ -5,9 +5,11 @@
 
         <?php get_template_part('template-parts/common/footer'); ?>
 
-        <?php wp_footer(); ?>
+        <?php 
+            wp_footer(); 
+            $MAPS_KEY = MAPS_KEY;
+        ?>
 
-        <?php $MAPS_KEY = "AIzaSyD-MXfDcED87UDyQ1QdAPn_7fz7_DgHGss"; ?>
         <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo $MAPS_KEY; ?>&callback=StoreLocator.init" async defer></script>
 
     </body>
